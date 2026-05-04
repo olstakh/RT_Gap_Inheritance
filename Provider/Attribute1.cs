1bc88ffac831ad6633bc0f2f76376f37889ebb16
Alex S <olstakh@microsoft.com>
2026-05-04 14:15:46 -0700

    Simplify constructor calling

--- Provider/Attribute1.cs
+++ Provider/Attribute1.cs
@@ -3,7 +3,7 @@ namespace Provider;
 [AttributeUsage(AttributeTargets.Class)]
 public sealed class Attribute1 : Attribute
 {
-    public Attribute1(params string[] x) : this(new ProviderDependency.Class1())
+    public Attribute1(params string[] x)
     {
     }
     public Attribute1(ProviderDependency.Class1 attribute)
